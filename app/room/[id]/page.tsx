68b293b4b2e8683729b4bd76926c057c6ddd497b
prathmesh <prathmeshpol61@gmail.com>
2023-10-04 19:57:57 +0530

    function change

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -3,17 +3,17 @@ import RoomDetail from "@/components/common/RoomDetail";
 type ParamsType = {
   params: { id: string };
 };
-async function getData(URL: string) {
-  const res = await fetch(URL);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
 export default async function page({ params }: ParamsType) {
   const { id } = params;
+  async function getData(URL: string) {
+    const res = await fetch(URL);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  }
   const data: HomeDataType = await getData(
     process.env.NEXT_PUBLIC_URL! + `/api/home/${id}`
   );
