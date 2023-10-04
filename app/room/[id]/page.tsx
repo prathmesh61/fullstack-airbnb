9e5b67a75d3293d1c5a7b303fe4bcf6eb987e62a
prathmesh <prathmeshpol61@gmail.com>
2023-10-04 20:27:39 +0530

    some changes

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -5,18 +5,6 @@ type ParamsType = {
 };
 export default async function page({ params }: ParamsType) {
   const { id } = params;
-  async function getData(URL: string) {
-    const res = await fetch(URL);
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    return res.json();
-  }
-  const data: HomeDataType = await getData(
-    process.env.NEXT_PUBLIC_URL! + `/api/home/${id}`
-  );
 
   return (
     <div className="p-4 border-b-2 border-gray-200">
